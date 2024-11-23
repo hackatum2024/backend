7c9219f39e6b10b61e1e92a4b39b8d32a4c21a91
Maximilian Förstl <maximilian.foerstl@st.oth-regensburg.de>
2024-11-23 16:18:15 +0100

    updated truncate

--- db/init/Truncate.sql
+++ db/init/Truncate.sql
@@ -2,6 +2,5 @@ CREATE OR REPLACE FUNCTION cleanUp(
         --> Truncates the table and resets the auto increment
         tableName VARCHAR(255)
     ) RETURNS void AS $$ BEGIN EXECUTE 'TRUNCATE TABLE ' || tableName || ' CASCADE';
-EXECUTE 'ALTER SEQUENCE ' || tableName || '_id_seq RESTART WITH 1';
 END;
 $$ LANGUAGE plpgsql;
